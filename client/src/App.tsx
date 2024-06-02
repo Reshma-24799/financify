b47788eec2c0e5568f6e206e5648c0080e8c04a3
RESHMA R <reshma24799@gmail.com>
2024-06-02 15:56:11 +0530

    completed the navbar section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,28 @@
 import {  ThemeProvider, createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
 
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
       <div className="app">
-        <ThemeProvider theme={theme}>
-           <CssBaseline />
-           hiii
-        </ThemeProvider>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 1rem">
+              <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard</div>} />
+              <Route path="/predictions" element={<div>predictions</div>} />
+            </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
+        
       </div>
   )
 }
