b3008953a3b57f20bdf82c8ebb4cfd7979204478
RESHMA R <reshma24799@gmail.com>
2024-06-03 15:27:35 +0530

    making it responsive

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,6 +1,6 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 
-const gridTemplate = `
+const gridTemplateLarge = `
   "a b c"
   "a b c"
   "a b c"
@@ -11,15 +11,54 @@ const gridTemplate = `
   "g h i"
   "g h j"
   "g h j"
-`
+`;
+const gridTemplateSmall = `
+ "a"
+ "a"
+ "a"
+ "a"
+ "b"
+ "b"
+ "b"
+ "b"
+ "c"
+ "c"
+ "c"
+ "d"
+ "d"
+ "d"
+ "e"
+ "e"
+ "f"
+ "f"
+ "f"
+ "g"
+ "g"
+ "g"
+ "h"
+ "h"
+ "h"
+ "h"
+ "i"
+ "i"
+ "j"
+ "j"
+`;
 const Dashboard = () => {
+  const isAboveMedium = useMediaQuery("(min-width: 1200px)");
   return (
     <Box 
       width="100%" height="100%" display="grid" gap="1.5rem"
-      sx={{
+      sx={
+          isAboveMedium ?
+        { 
         gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
         gridTemplateRows: "repeat(10, minmax(60px,1fr))",
-        gridTemplateAreas: gridTemplate
+        gridTemplateAreas: gridTemplateLarge
+      } : {
+        gridAutoColumns: "1fr",
+        gridAutoRows: "80px",
+        gridTemplateAreas: gridTemplateSmall
       }}
     >
       <Box  bgcolor="#fff" gridArea="a"></Box>
