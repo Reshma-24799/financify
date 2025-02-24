b7fb1f3c3f2c5c12c5fbeeee6d46c0ceedd5e84d
RESHMA R <reshma24799@gmail.com>
2025-02-24 12:59:37 +0100

    recharts - simpleAreaChart and styling

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -2,7 +2,7 @@ import express from 'express';
 import KPI from '../models/KPI.js';
 const router = express.Router();
 
-router.get("/kpi", async(req, res) => {
+router.get("/kpis", async(req, res) => {
     try{
         const kpis = await KPI.find();
         res.status(200).json(kpis);
