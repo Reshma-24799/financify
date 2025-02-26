06da60916789aeeaa427d6690bd9fa8874e41c94
RESHMA R <reshma24799@gmail.com>
2025-02-26 14:14:13 +0100

    Setting up transaction routes

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ import helmet from 'helmet';
 import morgan from "morgan";
 import kpiRoutes from './routes/kpi.js';
 import productRoutes from './routes/product.js';
+import transactionRoutes from './routes/transaction.js';
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
-import {kpis, products} from './data/data.js'
+import {kpis, products, transactions} from './data/data.js'
+import Transaction from "./models/Transaction.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -25,6 +27,7 @@ app.use(cors());
 /* ROUTES */
 app.use("/kpi",kpiRoutes)
 app.use("/product",productRoutes)
+app.use("/transaction",transactionRoutes)
 
 /* MONGOOSE */
 const PORT = process.env.PORT || 9000;
@@ -35,7 +38,8 @@ mongoose.connect(process.env.MONGO_URL)
     // add data one time only
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis)
-        // Product.insertMany(products)
+    // Product.insertMany(products)
+    // Transaction.insertMany(transactions)
 
 })
 .catch((err) => console.log(`${err} did not connect`))
